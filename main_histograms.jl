f7b2bc2a8e80c968cda7d3b47077b7b822880c5f
Alec <abills@andrew.cmu.edu>
2019-04-18 13:12:57 -0400

    Added Iteration Counter

--- main_histograms.jl
+++ main_histograms.jl
@@ -1,5 +1,6 @@
 include("airplane_header.jl")
 using DelimitedFiles
+using Printf
 num_sims=100000
 SE_RJ=zeros(num_sims)
 SE_NB=zeros(num_sims)
@@ -9,6 +10,9 @@ for n=1:num_sims
     SE_RJ[n]=SERP_Distributions(350,75,50000)
     SE_NB[n]=SERP_Distributions(500,150,100000)
     SE_WB[n]=SERP_Distributions(2000,300,250000)
+    if(n%10==0)
+    @printf("Iteration: %f  RJSE: %f  NBSE: %f  WBSE: %f",SE_RJ[n],SE_NB[n],SE_WB[n]); 
+    end
 end
 
 open("RJ_HISTOGRAM.csv","w") do io
